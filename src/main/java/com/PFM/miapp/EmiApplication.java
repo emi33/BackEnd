aa8fdbc9c977f097f48d9493c62059ab20b05ce1
emi33 <emiagustinchoque@gmail.com>
2023-04-11 06:59:46 -0300

    first commit

---
+++ src/main/java/com/PFM/miapp/EmiApplication.java
@@ -0,0 +1,13 @@
+package com.PFM.miapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmiApplication.class, args);
+	}
+
+}
